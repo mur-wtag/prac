0d4c3b6121bcb44d53ec5f370e701cd461fa2173
mur-wtag <mahbubur.rahman@welltravel.com>
2015-12-24 11:14:49 +0600

    Post request and endpoint added

--- app/api/api/resources/models.rb
+++ app/api/api/resources/models.rb
@@ -29,8 +29,24 @@ module API
                    type: String,
                    desc: 'Model Slug Type'
         end
-        post ':model_slug/model_types' do
-
+        post ':model_slug/model_types_price/:model_type_slug' do
+          # models = ::Models::CarModel.new(params[:model_slug]).results
+          model = Model.find_by_model_slug params[:model_slug]
+          attributes = {
+            name: 'Not Provided',
+            base_price: params[:base_price],
+            model_type_slug: params[:model_type_slug],
+            model_type_code: 'code_not_found'
+          }
+          model_type = {}
+          model_type = model.model_types.create attributes unless model.blank?
+          begin
+            present models, model_type, with: API::Entities::ModelType
+          rescue Exception => error
+            Rails.logger.error "#{error}"
+          status :bad_gateway
+          { errors: 'Something went wrong' }
+          end
         end
       end
     end
